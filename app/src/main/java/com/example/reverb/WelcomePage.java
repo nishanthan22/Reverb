d6f7088e6ee95f7565b8de829eab5302086926bc
AlisileAnusri <alisile.anusri@gmail.com>
2021-06-30 11:05:01 +0530

    HomePage Updates by Anu

---
+++ app/src/main/java/com/example/reverb/WelcomePage.java
@@ -0,0 +1,27 @@
+package com.example.reverb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomePage extends AppCompatActivity {
+    Button Homebtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+        Homebtn = findViewById(R.id.homepage);
+        Homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeintent = new Intent(WelcomePage.this,HomePage.class);
+                startActivity(homeintent);
+                finish();
+            }
+        });
+    }
+}
