19962b68055308581c66b9cd465a4b0a0a42a7e0
AlisileAnusri <alisile.anusri@gmail.com>
2021-06-30 12:06:17 +0530

    HomePage Updates by Anu

--- app/src/main/java/com/example/reverb/HomePage.java
+++ app/src/main/java/com/example/reverb/HomePage.java
@@ -2,13 +2,35 @@ package com.example.reverb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
-
+      Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        b1 = findViewById(R.id.AllSongs);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent allsongs = new Intent(HomePage.this,SongList.class);
+                startActivity(allsongs);
+                finish();
+            }
+        });
+        b2 = findViewById(R.id.MusicPlayer);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent audioplayer = new Intent(HomePage.this,AudioPlayer.class);
+                startActivity(audioplayer);
+                finish();
+            }
+        });
+
     }
 }
