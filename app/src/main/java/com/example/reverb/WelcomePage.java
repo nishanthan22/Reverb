3e33510eb94a56f9ca76140d9dae7f85ef73b36b
AlisileAnusri <alisile.anusri@gmail.com>
2021-07-02 00:58:23 +0530

    Just Tried

--- app/src/main/java/com/example/reverb/WelcomePage.java
+++ app/src/main/java/com/example/reverb/WelcomePage.java
@@ -2,13 +2,29 @@ package com.example.reverb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import android.content.Context;
 import android.content.Intent;
+//import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class WelcomePage extends AppCompatActivity {
     Button Homebtn;
+
+    //String prevStarted = "prevStarted";
+    //@Override
+    //protected void onResume() {
+        //super.onResume();
+        //SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+      //  if (!sharedpreferences.getBoolean(prevStarted, false)) {
+           // SharedPreferences.Editor editor = sharedpreferences.edit();
+            //editor.putBoolean(prevStarted, Boolean.TRUE);
+           // editor.apply();
+        //} else {
+            //finish();
+       // }
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
