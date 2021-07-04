dd09a40093db32a96d34aee004a4e6f08efc274c
Ria <riaammu2002@gmail.com>
2021-07-04 16:54:08 +0530

    Changes done by Ria.

--- app/src/main/java/com/example/reverb/HomePage.java
+++ app/src/main/java/com/example/reverb/HomePage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -69,5 +70,10 @@ public class HomePage extends AppCompatActivity {
              }
          });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
