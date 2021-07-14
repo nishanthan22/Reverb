ba73e09720aee1a2398864454ab42c93b8ea41d3
Ria <riaammu2002@gmail.com>
2021-07-14 14:28:41 +0530

    Changes done by Ria.

--- app/src/main/java/com/example/reverb/VideoPlayer.java
+++ app/src/main/java/com/example/reverb/VideoPlayer.java
@@ -3,9 +3,11 @@ package com.example.reverb;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentUris;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 
 //import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -16,11 +18,20 @@ import android.view.MenuItem;
 //import com.google.android.exoplayer2.ui.PlayerView;
 //import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 //import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import javax.sql.DataSource;
-
 public class VideoPlayer extends AppCompatActivity {
+
+    long videoId;
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
     //PlayerView playerView;
    // SimpleExoPlayer simpleExoPlayer;
     //int position = -1;
@@ -29,6 +40,9 @@ public class VideoPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+
+        initializeViews();
+        videoId = getIntent().getExtras().getLong("videoId");
        // position=getIntent().getIntExtra("position",-1);
 
         //String path = ModelVideo.getPath();
@@ -91,6 +105,64 @@ public class VideoPlayer extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void initializeViews() {
+        playerView = findViewById(R.id.playerView);
+    }
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+        Uri videoUri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, videoId);
+        MediaSource mediaSource = buildMediaSource(videoUri);
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, getString(R.string.app_name));
+        return new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+    }
+
+    private void releasePlayer(){
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Util.SDK_INT < 24 || player == null) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(Util.SDK_INT<24){
+            releasePlayer();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if(Util.SDK_INT>=24){
+            releasePlayer();
+        }
+        super.onStop();
     }
 
     @Override
